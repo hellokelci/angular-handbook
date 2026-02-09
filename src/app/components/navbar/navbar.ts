0e5e6e60dc9b4da6e8cd58f6573d942ac430d70d
hellokelci <kelcimensah@gmail.com>
2026-02-08 20:36:00 -0500

    added glass background to nav and image for home tab

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router,RouterLink, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'navbar',
@@ -7,4 +8,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isWhiteMode = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const url = event.urlAfterRedirects;
+      this.isWhiteMode = url === '/' || url === '/faqs';
+    });
+  }
+}
