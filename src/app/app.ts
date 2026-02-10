8712daeb58a96d858d7a367ddec38f82946bed33
Kelci Mensah <kelcimensah@gmail.com>
2026-02-09 20:23:42 -0500

    Added more components from Figma, almost complete Home a nd started on FAQs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Footbar } from './components/footbar/footbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-handbook');
+
+  protected readonly innerWidth = signal(window.innerWidth);
+
+  ngOnInit(): void {
+    this.innerWidth.set(window.innerWidth);
+  }
+
+  @HostListener('window:resize')
+  protected onResize() {
+    this.innerWidth.set(window.innerWidth);
+  }
 }
---
+++ src/app/components/footbar/footbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footbar',
+  imports: [],
+  templateUrl: './footbar.html',
+  styleUrl: './footbar.css',
+})
+export class Footbar {
+
+}
